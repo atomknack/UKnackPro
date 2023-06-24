5e0fa3bd915f64d0dd43370ba97e7c02c498d72b
atomknack <110350925+atomknack@users.noreply.github.com>
2023-06-25 01:41:52 +0400

    old code for pro concrete part

---
+++ Packages/com.atomknack.uknackpro/refactor/oldProConcrete/Concrete/UI/Dependants/UIUpdaterFromSOValue.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine.UIElements;
+using UKnack.SOValues;
+using UKnack.Values;
+using UKnack.Events;
+using UKnack.UI;
+using UnityEngine;
+
+namespace UKnack.Concrete.UI.Dependants
+{
+
+    public abstract class UIUpdaterFromSOValue<T> : Dependant
+    {
+        [SerializeField] private string _nameOfUpdatedUI;
+        private IValue<T> _counter;
+        private VisualElement _currentUpdatedUIElement;
+        protected VisualElement GetCurrentUpdatedUIElement => _currentUpdatedUIElement;
+        protected abstract IValue<T> GetValueProvider();
+        protected abstract string RawValueToStringConversion(T value);
+
+        protected override void OnLayoutCreatedAndReady(VisualElement layout)
+        {
+            _counter = GetValueProvider();
+            _currentUpdatedUIElement = layout.TryFindSomeKindOfTextStorage(_nameOfUpdatedUI);
+            UIContainerValidation(_currentUpdatedUIElement, _nameOfUpdatedUI);
+            UpdateUIBasedOnStorage(_counter.GetValue());
+            _counter.Subscribe(UpdateUIBasedOnStorage);
+        }
+
+        private void UpdateUIBasedOnStorage(T value)
+        {
+            _currentUpdatedUIElement.TryAssignTextWithoutNotification(RawValueToStringConversion(value));
+        }
+
+        protected override void OnLayoutReadyAndAllDependantsCalled(VisualElement layout)
+        {
+        }
+        protected override void OnLayoutGonnaBeDestroyedNow()
+        {
+            UIContainerValidation(_currentUpdatedUIElement, _nameOfUpdatedUI);
+            _counter.UnsubscribeNullSafe(UpdateUIBasedOnStorage);
+        }
+
+        private static void UIContainerValidation(VisualElement storage, string nameOfStorage)
+        {
+            if (string.IsNullOrEmpty(nameOfStorage))
+                throw new System.NullReferenceException($"name of storage is null or empty");
+            if (string.IsNullOrWhiteSpace(nameOfStorage))
+                throw new System.NullReferenceException($"name of storage is null or whitespaces");
+            if (storage == null)
+                throw new System.NullReferenceException($"Suitable text storage: {nameOfStorage} not found");
+        }
+    }
+}
