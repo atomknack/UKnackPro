24468b35876a1af20975c18ad4a2f55f688adcb1
atomknack <110350925+atomknack@users.noreply.github.com>
2023-08-01 14:10:39 +0400

    BoolValueDecomposer

---
+++ Packages/com.atomknack.uknackpro/UKnackProConcrete/Concrete/Commands/BoolValueDecomposer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UKnack.Commands;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UKnack.Concrete.Commands
+{
+    [Obsolete("Not tested")]
+    [AddComponentMenu("UKnack/Commands/BoolValueDecomposer")]
+    internal class BoolValueDecomposer : CommandMonoBehaviour<bool>
+    {
+        [SerializeField]
+        private UnityEvent<bool> _onTrue;
+
+        [SerializeField]
+        private UnityEvent<bool> _onFalse;
+
+        [SerializeField]
+        private UnityEvent<bool> _inverted;
+
+        [SerializeField]
+        private UnityEvent<bool> _unchanged;
+
+        public override void Execute(bool t)
+        {
+            if (t)
+                _onTrue?.Invoke(t);
+
+            if (!t)
+                _onFalse?.Invoke(t);
+
+            _inverted?.Invoke(!t);
+
+            _unchanged?.Invoke(t);
+        }
+    }
+}
