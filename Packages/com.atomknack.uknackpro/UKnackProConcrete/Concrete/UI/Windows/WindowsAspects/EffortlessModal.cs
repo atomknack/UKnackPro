325d3941a1b1b8a7a9104bb5a671142dd2484223
atomknack <110350925+atomknack@users.noreply.github.com>
2023-08-09 15:13:04 +0400

    Added EffortlessModal

---
+++ Packages/com.atomknack.uknackpro/UKnackProConcrete/Concrete/UI/Windows/WindowsAspects/EffortlessModal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UKnack.UI;
+using UKnack.UI.Windows.Aspects;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UKnack.Concrete.UI.Windows.WindowsAspects
+{
+    [AddComponentMenu("UKnack/UIWindowAspects/EffortlessModal")]
+    [RequireComponent(typeof(UIDocument))]
+    internal class EffortlessModal: MonoBehaviour, IModal, IOnScreenOrder
+    {
+        private UIDocument _document;
+
+        public float SortingOrder => _document.sortingOrder;
+
+        void OnEnable()
+        {
+            _document = GetComponent<UIDocument>();
+            _document.sortingOrder = IOnScreenOrder.NextFreeSpotOnTop();
+        }
+    }
+}
